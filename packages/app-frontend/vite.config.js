eb8149e01235e51dc1486e380c9eed034fbfbbde
Luis Sanchez Barranquero <luis.sb.dev@gmail.com>
2023-11-29 22:38:41 +0000

    fix: remove silly console logs

--- packages/app-frontend/vite.config.js
+++ packages/app-frontend/vite.config.js
@@ -6,9 +6,6 @@ import vue from '@vitejs/plugin-vue';
 const BACKEND_ENDPOINT =
   process.env.NODE_ENV === 'production' ? 'https://kino-app.onrender.com/' : 'http://localhost:3000/';
 
-console.log('Printing Base URL');
-console.log(BASE_URL);
-
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
