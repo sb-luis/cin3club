b7e766da15e314f2515a5e990c0ba1dc2717af7d
Luis Sanchez Barranquero <luis.sb.dev@gmail.com>
2023-12-08 04:30:50 +0000

    Revert "chore: add vike as an ssr dependency"
    This reverts commit 170f8b1838e0a1b6477d11212679cb19bb9ed75d.

--- packages/app-frontend/vite.config.js
+++ packages/app-frontend/vite.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vike from 'vike/plugin';
 
-export default {
-  plugins: [vue(), vike()],
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+});
