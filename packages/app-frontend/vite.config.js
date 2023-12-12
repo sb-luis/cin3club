bf28cbd1d7cd9a16db9ccf4e1bb1df13f2b64d4d
Luis Sanchez Barranquero <luis.sb.dev@gmail.com>
2023-12-12 00:08:37 +0000

    chore: build assets in workdir

--- packages/app-frontend/vite.config.js
+++ packages/app-frontend/vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    assetsDir: '.',
+  },
   server: {
     port: 8000,
     hmr: { host: '0.0.0.0' },
